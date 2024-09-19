2467b37e249325c7d6ef0918726373eccad1232d
OsicKwon <osic.kwon@gmail.com>
2024-09-18 23:13:45 -0400

    Update pagination.py

--- pagination.py
+++ pagination.py
@@ -8,21 +8,30 @@ cookies = {
 headers = {
     # headers
 }
+
 base_url = ""
 page_number: int = 1
 
-while True:
-    params = {
-        'pageNumber': page_number,
-    }
 
-    response = requests.get(base_url, params=params, cookies=cookies, headers=headers)
-    print(response.status_code)
+def main():
 
-    if response.status_code != 200:
-        break
-        
-    soup = BeautifulSoup(response.content, "html.parser")
-    print(soup.find_all('h1'))
+    while True:
+        params = {
+            'pageNumber': page_number,
+        }
+    
+        response = requests.get(base_url, params=params, cookies=cookies, headers=headers)
+        print(response.status_code)
+    
+        if response.status_code != 200:
+            break
+            
+        soup = BeautifulSoup(response.content, "html.parser")
+        print(soup.find_all('h1'))
+    
+        page_number += 1
 
-    page_number += 1
+
+if __name__ == "__main__":
+    main()
+    
